276795ea51f3c0f9a7dca67a7a2fcb8ebf792af8
bill417 <v.s.moisiadis@gmail.com>
2017-07-21 12:07:27 +0300

    gazebo_farm:Set the custom laser as primary

--- gazebo_farm.sh
+++ gazebo_farm.sh
@@ -7,4 +7,4 @@ sudo killall roscore
 sudo killall rosmaster
 sudo killall rosout
 
-roslaunch husky_gazebo husky_farm.launch laser_enabled:=true kinect_enabled:=true slam_laser_enabled:=false
+roslaunch husky_gazebo husky_farm.launch laser_enabled:=false kinect_enabled:=true slam_laser_enabled:=true
