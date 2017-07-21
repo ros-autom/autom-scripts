5d33417053c923cdd0d3b4e70647d7fe9f4e7d8d
dkati <dkatikaridis@gmail.com>
2017-07-21 11:24:58 +0300

    gazebo_farm:Also kill /rosout

--- gazebo_farm.sh
+++ gazebo_farm.sh
@@ -5,5 +5,6 @@ sudo killall gzclient
 sudo killall rviz
 sudo killall roscore
 sudo killall rosmaster
+sudo killall rosout
 
 roslaunch husky_gazebo husky_farm.launch laser_enabled:=true kinect_enabled:=true slam_laser_enabled:=false
