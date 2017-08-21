69ab67abd7031495a8d7eb566055ac3b91e49ea5
dkati <dkatikaridis@gmail.com>
2017-08-21 14:02:19 +0300

    showPlanners:Add exit prompt

--- showPlanners.sh
+++ showPlanners.sh
@@ -1,6 +1,28 @@
 red=`tput setaf 1`
 reset=`tput sgr0`
 
+exitRoutine() {
+	fuser -n tcp -k 11911
+	pkill -9 python
+	ps aux | grep -ie gzserver | awk '{print $2}' | xargs kill -9  || true
+	ps aux | grep -ie gzclient | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie rviz | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie roscore | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie rosmaster | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie rosout | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie roslaunch | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie tf2_relay_node | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie twist_mux | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie robot_state_publisher | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie master_sync | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie message_relay_node | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie move_base | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie map_server | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie static_transform_publisher | awk '{print $2}' | xargs kill -9   || true
+	ps aux | grep -ie amcl | awk '{print $2}' | xargs kill -9   || true
+	exit 1;
+}
+
  echo ""
  echo "For default usage (Untouched Eband planner):"
  echo ""
@@ -18,10 +40,8 @@ reset=`tput sgr0`
  echo "For AMCL, run: ${red}roslaunch husky_navigation amcl_demo_eband.launch${reset}"
  echo ""
 
-
-
  cd ..
- 
-
- echo ""
+ read -p "Exit? (y/n): " confirm && [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]] || exitRoutine;
 "$SHELL"
+
+
