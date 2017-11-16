4e977c7e9b52a0e5bb2d5293057ac33308ab8293
dkati <dkatikaridis@gmail.com>
2017-11-16 12:19:39 +0200

    Add gazebo_multihusky_karto_android.sh

---
+++ gazebo_multihusky_karto_android.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+sudo killall gzserver
+sudo killall gzclient
+sudo killall rviz
+sudo killall roscore
+sudo killall rosmaster
+
+
+roslaunch nre_simmultihusky autom_multihusky_karto.launch 
