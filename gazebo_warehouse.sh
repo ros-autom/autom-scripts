58ce893dbb994f0f2c6500f38be49bf64289ef21
v.s.moisiadis@gmail.com <dkatikaridis@gmail.com>
2017-11-28 10:38:20 +0200

    Release warehouse's data for image recognition
    also suggest user the processes

--- gazebo_warehouse.sh
+++ gazebo_warehouse.sh
@@ -17,6 +17,14 @@ sudo killall move_base
 sudo killall map_server
 sudo killall static_transform_publisher
 
+echo "==============="
+echo "For image recognition"
+echo "Run:"
+echo "${red}roslaunch find-object find_object_3d_warehouse{reset}"
+echo "And import the png files to rtab"
+echo "==============="
+echo ""
+
 gnome-terminal -e "roslaunch husky_gazebo husky_warehouse.launch laser_enabled:=false kinect_enabled:=true slam_laser_enabled:=true"
 
 #sleep 3
